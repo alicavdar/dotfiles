57c56ef8f9091a90331775dd60300112e503eb56
Ali Çavdar <779832+alicavdar@users.noreply.github.com>
2022-09-23 21:13:54 +0200

    Remove .git.local from bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,8 +13,6 @@ ln -sF $(PWD)/tigrc ~/.tigrc
 ln -sF $(PWD)/gitconfig ~/.gitconfig
 ln -sF $(PWD)/nvim ~/.config/nvim
 
-cp $(PWD)/gitconfig.local ~/.gitconfig.local
-
 # Install vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
