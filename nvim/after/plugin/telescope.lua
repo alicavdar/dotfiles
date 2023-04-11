dca3ce1e2ae6a7df3116332a01d0d73ac7fc6750
Ali Çavdar <779832+alicavdar@users.noreply.github.com>
2023-04-11 23:13:02 +0200

    Use telescope-live-grep-args

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,4 +1,5 @@
 local telescope = require("telescope")
+local extensions = telescope.extensions
 local builtin = require('telescope.builtin')
 local actions = require('telescope.actions')
 
@@ -11,11 +12,13 @@ telescope.setup({
   }
 })
 
+telescope.load_extension("live_grep_args")
+
 -- Enable line numbers
 vim.cmd "autocmd User TelescopePreviewerLoaded setlocal number"
 
 vim.keymap.set("n", "<leader>f", builtin.find_files)
-vim.keymap.set("n", "<leader>a", builtin.live_grep)
+vim.keymap.set("n", "<leader>a", extensions.live_grep_args.live_grep_args)
 vim.keymap.set("n", "<leader>g", builtin.git_status)
 vim.keymap.set("n", "<leader>t", builtin.filetypes)
 vim.keymap.set("n", "<C-p>", builtin.buffers)
--- nvim/lua/foamofthedays/packer.lua
+++ nvim/lua/foamofthedays/packer.lua
@@ -36,7 +36,10 @@ return require('packer').startup(function(use)
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.1',
   -- or                            , branch = '0.1.x',
-    requires = { {'nvim-lua/plenary.nvim'} }
+    requires = {
+      { 'nvim-lua/plenary.nvim' },
+      { "nvim-telescope/telescope-live-grep-args.nvim" },
+    }
   }
 
   use 'sainnhe/sonokai'
