c30ec858792edb99d1780a406ff3bbb00ae6a057
Ali Çavdar <779832+alicavdar@users.noreply.github.com>
2023-05-22 11:02:49 +0200

    Enable TreeSitter indentation

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -4,6 +4,10 @@ require('nvim-treesitter.configs').setup {
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
 
+  indent = {
+    enable = true,
+  },
+
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
