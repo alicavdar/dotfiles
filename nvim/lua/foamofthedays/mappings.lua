34d2f70eefd52397b6199d96a608ab6467750737
Ali Çavdar <779832+alicavdar@users.noreply.github.com>
2023-11-01 14:42:54 +0100

    Create NVIM bindings for quickfix

--- nvim/lua/foamofthedays/mappings.lua
+++ nvim/lua/foamofthedays/mappings.lua
@@ -80,3 +80,6 @@ vim.keymap.set("n", "<Space><Space>", ":lua print(vim.fn.expand('%'))<CR>")
 
 -- Format the current buffer
 vim.keymap.set("n", "<Leader>p", ":Neoformat<CR>")
+
+vim.keymap.set("n", "<C-]>", ":cnext<CR>")
+vim.keymap.set("n", "<C-[>", ":cprev<CR>")
