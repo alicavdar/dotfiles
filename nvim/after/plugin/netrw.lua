cad43a9567cda776e43cc395bed6b57b9d4d9e93
Ali Çavdar <779832+alicavdar@users.noreply.github.com>
2023-11-22 10:54:54 +0100

    Close netrw buffers automatically

--- nvim/after/plugin/netrw.lua
+++ nvim/after/plugin/netrw.lua
@@ -1,3 +1,7 @@
+-- Close netrw buffers automatically
+-- https://github.com/tpope/vim-vinegar/issues/13
+vim.g.netrw_fastbrowse = 0
+
 -- Show line numbers
 vim.g.netrw_bufsettings = 'noma nomod nu nowrap ro nobl'
 vim.api.nvim_exec([[
