c37720f59d299e6d301215cbd42eaa63f8ca46ec
Ali Cavdar <779832+alicavdar@users.noreply.github.com>
2024-02-21 10:08:20 +0100

    Bump Neovim plugins

--- nvim/lua/foamofthedays/plugins/telescope.lua
+++ nvim/lua/foamofthedays/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-telescope/telescope.nvim",
-  tag = "0.1.x",
+  tag = "0.1.5",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope-live-grep-args.nvim",
