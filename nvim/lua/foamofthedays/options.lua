8a84bd2cc30940d62093a7cf639b033f90395be6
Ali Cavdar <779832+alicavdar@users.noreply.github.com>
2024-02-29 12:57:33 +0100

    Update the completion settings

--- nvim/lua/foamofthedays/options.lua
+++ nvim/lua/foamofthedays/options.lua
@@ -48,12 +48,8 @@ vim.opt.foldlevel = 20 -- Opens all folds when opening a file
 vim.opt.foldmethod = 'expr'
 vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
 
--- Completion
-vim.opt.completeopt = { "noinsert", "menuone", "noselect" }
-vim.opt.wildmenu    = true
-vim.opt.wildmode    = "longest:full,full"
-vim.opt.wildoptions = "pum" -- Show completion items using the pop-up-menu (pum)
-vim.opt.pumblend    = 15 -- completion menu transparency
+-- Completion menu transparency
+vim.opt.pumblend = 15
 
 -- Undo history
 vim.opt.undofile = true
--- nvim/lua/foamofthedays/plugins/lsp.lua
+++ nvim/lua/foamofthedays/plugins/lsp.lua
@@ -84,9 +84,7 @@ return {
         end,
       },
       mapping = cmp.mapping.preset.insert({
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-p>'] = cmp.mapping.select_prev_item(),
-        ['<C-n>'] = cmp.mapping.select_next_item()
+        ['<CR>'] = cmp.mapping.confirm({ select = true })
       }),
       sources = cmp.config.sources({
         { name = 'nvim_lsp' },
