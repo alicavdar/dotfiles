258aa93e77bb697fd280dbd02673be9236ec8e0d
Ali Cavdar <779832+alicavdar@users.noreply.github.com>
2024-09-20 12:58:30 +0200

    Do not use Treesitter's experimental indentation

--- nvim/lua/foamofthedays/plugins/treesitter.lua
+++ nvim/lua/foamofthedays/plugins/treesitter.lua
@@ -14,10 +14,6 @@ return {
       -- Install parsers synchronously (only applied to `ensure_installed`)
       sync_install = false,
 
-      indent = {
-        enable = true,
-      },
-
       -- Automatically install missing parsers when entering buffer
       -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
       auto_install = true,
