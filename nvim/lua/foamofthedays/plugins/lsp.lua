b4115fb9e70a8fe9f44742b78258a24347757632
Ali Cavdar <779832+alicavdar@users.noreply.github.com>
2024-09-30 15:30:02 +0200

    Hide lines in telescope's lsp references

--- nvim/lua/foamofthedays/plugins/lsp.lua
+++ nvim/lua/foamofthedays/plugins/lsp.lua
@@ -83,7 +83,11 @@ return {
       vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
       vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, opts)
-      vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, opts)
+      vim.keymap.set('n', 'gr', function ()
+        return require('telescope.builtin').lsp_references({
+          show_line = false,
+        })
+      end, opts)
       vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
       vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
       vim.keymap.set('n', 'g[', vim.diagnostic.goto_prev, opts)
