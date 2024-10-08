28797384bb400ce0abeca655a567fe399c72d36e
Ali Cavdar <779832+alicavdar@users.noreply.github.com>
2024-10-08 21:29:21 +0200

    Show ~ in the prompt if it's $HOME

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -8,11 +8,17 @@ function prepare_zsh_prompt() {
   ref=$(command git symbolic-ref HEAD 2>/dev/null) ||
     ref=$(command git rev-parse --short HEAD 2>/dev/null)
 
-  if [[ -z "$ref" ]]; then
-    echo "$(basename $(pwd)) "
-  else
+  if [[ -n "$ref" ]]; then
     echo "$(zsh_git_prompt) "
+    return 0
   fi
+
+  if [[ "$HOME" == "$(pwd)" ]]; then
+    echo "~ "
+    return 0
+  fi
+
+  echo "$(basename $(pwd)) "
 }
 
 function zsh_git_prompt() {
