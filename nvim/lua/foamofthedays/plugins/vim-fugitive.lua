9e6b1c181570d2448f3dab8178603186ac30ec2b
Ali Cavdar <779832+alicavdar@users.noreply.github.com>
2025-01-24 15:09:56 +0100

    Remove unused fugitive bindings

--- nvim/lua/foamofthedays/plugins/vim-fugitive.lua
+++ nvim/lua/foamofthedays/plugins/vim-fugitive.lua
@@ -2,10 +2,5 @@ return {
   "tpope/vim-fugitive",
   config = function()
     vim.api.nvim_create_user_command("Blame", ":G blame", {})
-    vim.api.nvim_create_user_command("Gblame", ":G blame", {})
-    vim.api.nvim_create_user_command("GBlame", ":G blame", {})
-
-    vim.api.nvim_create_user_command("Browse", ":G browse", {})
-    vim.api.nvim_create_user_command("GBrowse", ":G browse", {})
   end
 }
