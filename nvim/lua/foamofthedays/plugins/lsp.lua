82feaf1c259e56fb0a7184a84b26b4edd26519d4
Ali Cavdar <779832+alicavdar@users.noreply.github.com>
2025-08-20 23:34:29 +0200

    Reconfigure LSP

--- nvim/lua/foamofthedays/plugins/lsp.lua
+++ nvim/lua/foamofthedays/plugins/lsp.lua
@@ -1,66 +1,30 @@
 local ensure_installed = {
   'gopls', 'html', 'eslint', 'ts_ls',
   'lua_ls', 'elixirls', 'tailwindcss', 'clangd',
-  'terraformls', 'denols'
+  'terraformls',
 }
-
-local server_configs = {
-  lua_ls = {
-    settings = {
-      Lua = {
-        diagnostics = {
-          globals = { "vim" },
-        },
-        hint = { enable = true },
-      }
-    }
-  },
-
-  ts_ls = function (lspconfig)
-    return {
-      root_dir = lspconfig.util.root_pattern("package.json"),
-      single_file_support = true,
-    }
-  end,
-
-  denols = function (lspconfig)
-    return {
-      root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
-    }
-  end
-}
-
-local function get_server_config(server_name, lspconfig)
-  local config = server_configs[server_name]
-
-  if not config then
-    return nil
-  end
-
-  if type(config) == "function" then
-    return config(lspconfig)
-  end
-
-  return config
-end
 
 return {
   "neovim/nvim-lspconfig",
   dependencies = {
-    "saghen/blink.cmp",
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
+    "mason-org/mason.nvim",
+    "mason-org/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
     "j-hui/fidget.nvim",
   },
   config = function()
-    local lspconfig = require("lspconfig")
-    local blink_cmp = require("blink.cmp")
+    require("fidget").setup()
+    require("mason").setup()
+    require("mason-lspconfig").setup()
 
-    local base_server_config = {
-      capabilities = blink_cmp.get_lsp_capabilities(),
-      on_attach = function (_, bufnr)
-        local opts = {buffer = bufnr, remap = false}
+    require("mason-tool-installer").setup({
+      ensure_installed = ensure_installed
+    })
 
+    vim.api.nvim_create_autocmd('LspAttach', {
+      group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+      callback = function(ev)
+        local opts = { buffer = ev.buf, remap = false }
         vim.keymap.set('n', '<C-e>', vim.diagnostic.open_float, opts)
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
         vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
@@ -72,22 +36,13 @@ return {
         vim.keymap.set('n', 'g[', vim.diagnostic.goto_prev, opts)
         vim.keymap.set('n', 'g]', vim.diagnostic.goto_next, opts)
       end,
-    }
+    })
 
-    require("fidget").setup({})
-    require("mason").setup()
-    require("mason-lspconfig").setup({
-      ensure_installed = ensure_installed,
-      handlers = {
-        function(server_name)
-          local server_config = vim.tbl_deep_extend(
-            "force",
-            base_server_config,
-            get_server_config(server_name, lspconfig) or {}
-          )
-
-          lspconfig[server_name].setup(server_config)
-        end,
+    vim.lsp.config('lua_ls', {
+      settings = {
+        Lua = {
+          diagnostics = { globals = { "vim" } },
+        }
       }
     })
 
