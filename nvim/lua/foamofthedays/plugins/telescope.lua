00a10507436a6d2e1eb9e4d8ecd2e08f50e28663
Ali Cavdar <779832+alicavdar@users.noreply.github.com>
2025-11-28 14:07:19 +0100

    Add error handling for telescope.builtin.git_status keybinding

--- nvim/lua/foamofthedays/plugins/telescope.lua
+++ nvim/lua/foamofthedays/plugins/telescope.lua
@@ -36,7 +36,14 @@ return {
 
     vim.keymap.set("n", "<leader>f", builtin.find_files)
     vim.keymap.set("n", "<leader>a", extensions.live_grep_args.live_grep_args)
-    vim.keymap.set("n", "<leader>g", builtin.git_status)
+
+    vim.keymap.set("n", "<leader>g", function ()
+      local ok = pcall(builtin.git_status)
+      if not ok then
+        print("Not a git repository")
+      end
+    end)
+
     vim.keymap.set("n", "<leader>t", builtin.filetypes)
     vim.keymap.set("n", "<C-p>", builtin.buffers)
     vim.keymap.set("n", "<leader>s", builtin.lsp_document_symbols)
